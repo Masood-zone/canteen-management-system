0f7d2e3671729a028c12226bae591a5376597aec
masood <masoodacheampong@gmail.com>
2024-11-06 13:43:39 +0000

    made changes to the login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -18,7 +18,7 @@ export default function Login() {
     // Here you would typically handle the login logic
     console.log("Login attempt with:", { email, password });
     // For demonstration, we'll just redirect to the dashboard
-    router.push("/");
+    router.push("/dashboard");
   };
 
   return (
@@ -27,14 +27,16 @@ export default function Login() {
         <div className="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
           <div className="rounded-3xl border border-gray-100 bg-white dark:bg-gray-800 dark:border-gray-700 shadow-2xl shadow-gray-600/10 backdrop-blur-2xl">
             <div className="p-8 py-12 sm:p-16">
-              <div className="space-y-4">
-                <Image
-                  src={GreeLogo}
-                  width={144}
-                  height={30}
-                  className="w-36"
-                  alt="Canteen logo"
-                />
+              <div className="space-y-4 flex flex-col items-center justify-center">
+                <div className="w-24 h-24 overflow-hidden rounded-full">
+                  <Image
+                    src={GreeLogo}
+                    width={144}
+                    height={30}
+                    className=""
+                    alt="Canteen logo"
+                  />
+                </div>
                 <h2 className="mb-8 text-2xl font-bold text-gray-800 dark:text-white">
                   Sign in to your account
                 </h2>
@@ -54,7 +56,7 @@ export default function Login() {
                     autoComplete="username"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="focus:outline-none block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                    className="focus:outline-none block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300"
                     required
                   />
                 </div>
@@ -78,24 +80,19 @@ export default function Login() {
                     autoComplete="current-password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="focus:outline-none block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300 invalid:ring-2 invalid:ring-red-400 focus:ring-2 focus:ring-cyan-300"
+                    className="focus:outline-none block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-transparent px-4 py-3 text-gray-600 transition duration-300"
                     required
                   />
                 </div>
 
-                <Button
-                  type="submit"
-                  className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
-                >
-                  <span className="relative text-base font-semibold text-white dark:text-dark">
-                    Connect
-                  </span>
+                <Button type="submit" className="w-full py-6">
+                  Connect
                 </Button>
               </form>
             </div>
           </div>
           <div className="space-x-4 text-center text-gray-500">
-            <span>&copy; Canteen</span>
+            <span>&copy;CMS</span>
             <Link href="/contact" className="text-sm hover:text-primary">
               Contact
             </Link>
